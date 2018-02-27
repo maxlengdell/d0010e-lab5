dcaf5ff168eeee72f75f6aa15bc349a9918783f3
Jarani1 <shahin.salehi1@outlook.com>
2018-02-27 10:49:13 +0100

    customer construct and set quetime

---
+++ src/supermarket/customer.java
@@ -0,0 +1,13 @@
+package supermarket;
+
+public class customer {
+    int id;
+    double que;
+    public customer(int id){
+        // When you create a customer object you assign it an customer id
+    }
+
+    public void setQue(double time){
+        this.que=time;
+    }
+}
