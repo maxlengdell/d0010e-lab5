d90e3665bbd5446803e986a6d9dd3afed50f0280
Jarani1 <shahin.salehi1@outlook.com>
2018-02-27 11:04:26 +0100

    customer que methods

--- src/supermarket/Customer.java
+++ src/supermarket/Customer.java
@@ -1,9 +1,15 @@
 package supermarket;
 
+/**
+ *
+ * @author Shahin, Max, Johan, Linus
+ *
+ */
+
 public class Customer {
     int id;
     double timeEnt;
-    double que;
+    double queEnt;
     public Customer(int id, double timeEnt){
         // When you create a customer object you assign it an customer id
         this.id = id;
@@ -12,7 +18,15 @@ public class Customer {
 
     public double custExit(double time){
         //return total time in store
-        return this.timeEnt - time;
+        return  time-this.timeEnt;
+    }
+    // customer enter queue
+    public void enterQue(double time){
+        this.queEnt=time;
+    }
+    //customer exit queue return time in queue
+    public double exitQue(double time){
+        return time-this.queEnt;
     }
 
 
