87537c67ed60cbd522583a0f3fa8530f0ca85265
maxlengdell <max.lengdell@gmail.com>
2018-02-27 11:23:18 +0100

    return of the jedi

--- src/simulator/Event.java
+++ src/simulator/Event.java
@@ -6,5 +6,28 @@ public class Event {
 		
 		
 	}
+	public void open() {
+		//Open shop and lets customers in. 
+		
+	}
+	public void close() {
+		//Reject customers from entering the shop. Let customers already in the shop finish their shopping. 
+	}
+	public void custArrives() {
+		//Initierar
+		//customer creates new object  to customer. Waiting for method name.
+		//Takes two arguments. 
+	}
+	public void custEnterQueue() {
+		//Calls method in customer. The time that is returned 
+		//customerExit returns time units the customer has been in queue. 
+		
+	}
+	public void custLeaves() {
+		//Get a timestamp of when the customer leaves.
+		
+	}public void custLeaveQueue() {
+		
+	}
 
 }
