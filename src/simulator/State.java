8aab2dc3cb01ce9e0a359d3745bf7b832447454d
Linus <limpan.ml@hotmail.com>
2018-02-27 12:27:51 +0100

    Updates to State

--- src/simulator/State.java
+++ src/simulator/State.java
@@ -3,8 +3,9 @@ package simulator;
 import java.util.Observable;
 
 public class State extends Observable{
-	public double time;
-	public boolean isActive;
+	private double time;
+	private boolean isActive;
+	private Event currentEvent; 
 	public State(double time, boolean isActive){
 		this.time=time;
 		this.isActive=isActive;
@@ -14,6 +15,12 @@ public class State extends Observable{
 	}
 	public boolean getisActive(){
 		return this.isActive;
+	}
+	public Event getCurrentEvent(){
+		return this.currentEvent;
+	}
+	public void changeEvent(Event newEvent){
+		this.currentEvent=newEvent;
 	}
 	
 	public void notifyStateObs(){
