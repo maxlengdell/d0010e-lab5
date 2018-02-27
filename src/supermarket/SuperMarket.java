9d079588756c31e0a09c1b1235e7c076845ba50c
Linus <limpan.ml@hotmail.com>
2018-02-27 11:11:16 +0100

    Supermarket update

--- src/supermarket/SuperMarket.java
+++ src/supermarket/SuperMarket.java
@@ -1,5 +1,42 @@
 package supermarket;
 
-public class SuperMarket {
+import java.util.ArrayList;
+
+import simulator.State;
+
+public class SuperMarket extends State {
+	public boolean isOpen=false;
+	public ArrayList <Customer> Queue;
+	public double skill;
+	public int missedCustomer=0;
+	public int debitedCustomer;
+	public int[] cashRegister={0,0,0,0};
+	public int maxCustomerAmount=5;
+	public int currentCustomerAmount=0;
+	public double waitedTimed=0;
+	public int CustomerIDCount=1;
+	
+	public SuperMarket(){
+		super(0, true);
+		
+	}
+	public void getOpenState(){
+		
+	}
+	public void getClosedState(){
+		
+	}
+	public void getCustomerPayedState(){
+		
+	}
+	public void getCustomerEnteredState(){
+		
+	}
+	public void getCustomerLeftState(){
+		
+	}
+	public void changeState(){
+		
+	}
 
 }
