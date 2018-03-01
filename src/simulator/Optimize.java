d98ca13775627c6144c3a0c00e4e2fa6c353179f
maxlengdell <max.lengdell@gmail.com>
2018-03-01 11:50:14 +0100

    Optimize update

--- src/simulator/Optimize.java
+++ src/simulator/Optimize.java
@@ -1,5 +1,27 @@
 package simulator;
 
-public class Optimize {
+import supermarket.SuperMarket;
+import random.ExponentialRandomStream;
 
+public class Optimize {
+	private static SuperMarket supermarket;
+
+	State S;
+	EventQueue EQ;
+	
+	public static void cashOpen() {
+		for (int i = 0; i < supermarket.getmaxCustomerAmount(); i++) {
+			
+			
+			setNumberOfCashRegisters(i);
+			while(S.getisActive()) {
+				EQ.eventList.get(0).Execute();
+			}
+			
+		}
+	}
+	public static void main(String[] args) {
+		ExponentialRandomStream exp = new ExponentialRandomStream(1,1234);
+		System.out.println("rand valie from optimize: "+ exp.next());
+	}
 }
