78619c151fc58399a4584c986f4cb66319fd1fc3
Linus <limpan.ml@hotmail.com>
2018-03-01 11:30:59 +0100

    Updates

--- src/supermarket/EventDebiting.java
+++ src/supermarket/EventDebiting.java
@@ -8,30 +8,29 @@ public class EventDebiting extends Event {
 	double time;
 	SuperMarket s;
 	Customer c;
-	
-	
 	public EventDebiting(double time, SuperMarket s, Customer c){
 		super(time,s);
 		this.s=s;
-		this.c=c;
-		
+		this.c=c;	
 	}
 	public void Execute() {
-		int[] cashReg=s.getcashRegister();
-	 
 		//Time for how long the customer shopped*I think*(check)
 		//Puts the customer in the supermarket queue.
-	 for(int i=0; i<=cashReg.length; i++){
-		if(s.cashRegStatus()==0) {//if cashregister is free. Execute this. 
-			s.getEventQueue().addEvent(s.getCurrentEvent());
+		if(s.cashRegStatus()==true) {//if cashregister is free. Execute this. 
+			
+			double departureTime=time;
+			time+s.getRandomNumber()/s.getSkill();
+			s.getEventQueue().addEvent(new EventDeparture(departureTime));
 			s.addCustomerToQueue(c);
+			s.takeCashRegister();
+			s.
 			//Place customer in the cashregister. 
 		
 		}
-		else if(s.cashRegStatus()==1){ //if cashregister is occupied. Execute this.
-			s.addCustomerToQueue(c);
-		}
 		
-	 }
+		if(s.cashRegStatus()==false){ //if cashregister is occupied. Execute this.
+			s.addCustomerToQueue(c);
+			
+		}
 	}
 }
