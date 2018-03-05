6381858d65caa5d195592472fdb22bf13920be02
Linus <limpan.ml@hotmail.com>
2018-03-05 09:59:57 +0100

    Update

--- src/simulator/Event.java
+++ src/simulator/Event.java
@@ -10,20 +10,33 @@ public abstract class Event {
 	private double time;
 	private State state;
 	/**Constructor for the class Event. A time of event and a state of event is assigned when a event object is created*/
+	/**
+	 * 
+	 * @param time
+	 * @param state
+	 */
 	public Event(double time, State state) { //Perhaps more parameters?
 		
 		this.time = time; //Sort the events by time.
 		this.state = state;
 	}
 	/**This method returns the time*/
+	/**
+	 * 
+	 * @return
+	 */
 	public double getTime() {
 		return time;
 	}
 	/**This method returns the state/what kind of event it is*/
+	/**
+	 * 
+	 * @return
+	 */
 	public State getState() {
 		return state;
 	}
-	/**this method runs an event*/
+	/**This method runs an event*/
 	public void execute() {
 		
 	}
--- src/supermarket/Customer.java
+++ src/supermarket/Customer.java
@@ -15,8 +15,13 @@ public class Customer {
     double exitStore;
     // When you create a customer object you assign it a customer id and time of entry
     /**Constructor for the class Customer. When a customer object is created it is assigned a customer ID and 
-     * a time of entering the supermarket.
+     * a time of entering the supermarket. /**
      * 
+     */
+    /**
+     * 
+     * @param id
+     * @param timeEnt
      */
     public Customer(int id, double timeEnt){
         this.id = id;
@@ -25,18 +30,33 @@ public class Customer {
 
     //time roaming in supermarket, time enter until in queue == roam
     /**This method returns the time that a customer is roaming the supermarket until the customer enters the cash register queue*/
+    /**
+     * 
+     * @param Roamtime
+     * @return
+     */
     public double roaming(double Roamtime){
         return Roamtime - this.timeEnt;
     }
 
     //return total time in store
     /**This method returns how long the customer has been in the supermarket when leaving*/
+    /**
+     * 
+     * @param time
+     * @return
+     */
     public double custExit(double time){
         exitStore = time-timeEnt;
         return exitStore;
     }
     // customer enter queue this ends roam and returns it. Could change this and make it separate.
     /**This method makes a customer enter a cash register queue. It also returns the time a customer has spent roaming*/
+    /**
+     * 
+     * @param time
+     * @return
+     */
     public double enterQue(double time){
         double roamTime = roaming(time);
         queEnt=time;
@@ -44,6 +64,11 @@ public class Customer {
     }
     //customer exit queue return time in queue
     /**This method returns the time a customer has spent in the queue and also makes a customer exit the supermarket*/
+    /**
+     * 
+     * @param time
+     * @return
+     */
     public double exitQue(double time){
         exitQue = time-queEnt;
         return exitQue;
@@ -51,10 +76,18 @@ public class Customer {
 
     //time paying and scanning: from exit que to exit store. Nullpointer dangerZONE
     /**This method returns the time spent paying in the cash register*/
+    /**
+     * 
+     * @return
+     */
     public double getPaytime(){
         return exitStore - exitQue;
     }
     /**This method returns the customer ID*/
+    /**
+     * 
+     * @return
+     */
     public int getID(){
         return id;
     }
